5e691d6d9a5ad45047db402bbe9b1e27ef8fac2c
voldo <filipecavoldo@outlook.com>
2026-01-08 19:45:39 -0300

    first commit

---
+++ eleventy.config.js
@@ -0,0 +1,5 @@
+export default function(eleventyConfig) {
+  eleventyConfig.setInputDirectory("src");
+  eleventyConfig.setOutputDirectory("out");
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+};
