f3e8a5e67a756b1d6d5dd3ebb19c7076859d8b11
voldo <filipecavoldo@outlook.com>
2026-01-09 01:00:36 -0300

    pages

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,13 @@
 import pluginTOC from "eleventy-plugin-toc";
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
-export default function(eleventyConfig) {
+export default function (eleventyConfig) {
     eleventyConfig.setInputDirectory("src");
     eleventyConfig.setOutputDirectory("docs");
     eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addPlugin(pluginTOC);
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+    return {
+        pathPrefix: "/tales-of-data-tables/"
+    };
 };
